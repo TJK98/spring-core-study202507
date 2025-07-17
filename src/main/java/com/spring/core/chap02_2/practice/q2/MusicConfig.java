cc0fae6832b05d7a26744715e4b93843ab4f453d
TJK98 <wtj1998@naver.com>
2025-07-17 16:07:14 +0900

    음악플레이어와 스피커 클래스 생성 및 의존성 주입을 위한 MusicConfig 및 MusicPlayerTest 추가, 스프링 빈 활용 예제 구현

---
+++ src/main/java/com/spring/core/chap02_2/practice/q2/MusicConfig.java
@@ -0,0 +1,23 @@
+package com.spring.core.chap02_2.practice.q2;
+
+import com.spring.core.chap02_2.practice.q2.MusicPlayer;
+import com.spring.core.chap02_2.practice.q2.Speaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MusicConfig {
+
+    @Bean
+    public Speaker speaker() {
+        return new Speaker();
+    }
+
+    @Bean
+    public MusicPlayer musicPlayer() {
+        MusicPlayer musicPlayer = new MusicPlayer();
+        musicPlayer.setSpeaker(speaker());
+
+        return musicPlayer;
+    }
+}
