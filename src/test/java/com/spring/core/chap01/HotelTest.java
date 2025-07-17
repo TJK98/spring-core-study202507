18f081eedd2275c09a71302931c34d3045929dd9
TJK98 <wtj1998@naver.com>
2025-07-17 12:49:18 +0900

    호텔, 레스토랑, 셰프, 코스 관련 기본 클래스 및 인터페이스 추가, 호텔 의존성 주입 구현 및 호텔 관리 로직 변경 및 HotelManager 도입.

---
+++ src/test/java/com/spring/core/chap01/HotelTest.java
@@ -0,0 +1,19 @@
+package com.spring.core.chap01;
+
+import com.spring.core.chap01.manage.HotelManager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelTest {
+
+    // 테스트 메서드
+    @Test // 단위 테스트
+    void hotel() {
+
+        // 호텔객체를 생성- 매니저한테 문의
+        HotelManager manager = new HotelManager();
+        Hotel hotel = manager.hotel();
+        hotel.reserve();
+    }
+}
